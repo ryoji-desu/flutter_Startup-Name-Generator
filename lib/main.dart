40a3808fbaef2298d9a4eba6ae5bd47b3ff5546e
ryouji nagai <ryoujinagai@CBcloudnoMacBook-Pro.local>
2020-07-11 11:42:23 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,69 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'dart:math' as math;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final String title = 'Color Panel';
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: title,
       theme: ThemeData(
-        primaryColor: Colors.white,
+        primarySwatch: Colors.blue,
       ),
-      home: RandomWords(),
+      home: MyHome(),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  @override
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-                (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile
-              .divideTiles(
-            context: context,
-            tiles: tiles,
-          )
-              .toList();
+class MyHome extends StatelessWidget {
+  final String title = 'Color Panel';
+  final int dividedNum = 3;
 
-          return Scaffold(         // Add 6 lines from here...
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );                       // ... to here.
-        },
-      ),
-    );
-  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
+    final Size size               = MediaQuery.of(context).size;
+    final double width            = size.width;
+    final double height           = size.height;
+    final double rectangleSize    = width / dividedNum;
+    int columnLength        = height ~/ rectangleSize;
+    if (height*columnLength > height) columnLength -= 1;
 
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+    return Scaffold(
+      appBar: AppBar(title: Text(title),),
+      body: SafeArea(
+        child: Column(
+          children: List.generate(columnLength, (i) => row(rectangleSize: rectangleSize)),
+        ),
       ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
     );
   }
-}
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
+
+  Container row({rectangleSize = 80,}) {
+    final colorMaster = [Colors.blueAccent, Colors.lightGreenAccent, Colors.redAccent, Colors.green, Colors.yellowAccent, Colors.deepPurpleAccent, Colors.pinkAccent, Colors.orangeAccent, Colors.indigoAccent,];
+
+    List colorList = [];
+    for(var s = 0; s < dividedNum; s++) {
+      var rand = new math.Random();
+      var colorMasterLength = colorMaster.length;
+      var randomInt = rand.nextInt(colorMasterLength - 1);
+      colorList.add(colorMaster[randomInt]);
+    }
+
+    return Container(
+      child: Row(
+        children: List.generate(dividedNum, (i) => rectangle(size: rectangleSize, color: colorList[i])),
+      ),
+    );
+  }
+
+  Container rectangle({size = 80, color = Colors.green}) {
+    return Container(
+      width: size,
+      height: size,
+      color: color
+    );
+  }
 }
